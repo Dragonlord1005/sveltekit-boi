2f2c33d8b1240e99ebd405e9d981f2c02a8551eb
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-03-05 17:30:22 -0700

    sup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
 <h1>SvelteKit Boi</h1>
+<a href="/about">About</a>
 
 <style>
     h1 {
         text-align: center;
+    }
+
+    a {
+        text-decoration: none;
+        color: black
+    }
+    a:hover {
+        color: #facc15;
+        transition-duration: 2s;
     }
 </style>
