9a816e7e722ec7e63f9f9222b2868ae4e80c7d62
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-05-17 18:46:43 +0000

    🎨Formatted code

--- src/routes/Counter.test.ts
+++ src/routes/Counter.test.ts
@@ -4,30 +4,29 @@ import userEvent from '@testing-library/user-event';
 import Counter from './Counter.svelte';
 
 describe('Counter testing', () => {
-test('renders', () => {
-  // Imports
-  const { getByLabelText, getByTestId } = render(Counter);
-  // Defines the elements to be tested
-  const decreaseButton = getByLabelText('Decrease the counter by one');
-  const increaseButton = getByLabelText('Increase the counter by one');
-  const counterDigits = getByTestId('counter-digits');
+	test('renders', () => {
+		// Imports
+		const { getByLabelText, getByTestId } = render(Counter);
+		// Defines the elements to be tested
+		const decreaseButton = getByLabelText('Decrease the counter by one');
+		const increaseButton = getByLabelText('Increase the counter by one');
+		const counterDigits = getByTestId('counter-digits');
 
-  // Checks if the elements are in the document
-  expect(decreaseButton).toBeInTheDocument();
-  expect(increaseButton).toBeInTheDocument();
-  expect(counterDigits).toBeInTheDocument();
-});
+		// Checks if the elements are in the document
+		expect(decreaseButton).toBeInTheDocument();
+		expect(increaseButton).toBeInTheDocument();
+		expect(counterDigits).toBeInTheDocument();
+	});
 
-test('increases count', async () => {
-  const { getByLabelText, getByText } = render(Counter);
+	test('increases count', async () => {
+		const { getByLabelText, getByText } = render(Counter);
 
-  const increaseButton = getByLabelText('Increase the counter by one');
+		const increaseButton = getByLabelText('Increase the counter by one');
 
-  expect(getByText('0')).toBeInTheDocument();
+		expect(getByText('0')).toBeInTheDocument();
 
-  await userEvent.click(increaseButton);
+		await userEvent.click(increaseButton);
 
-  expect(getByText('1')).toBeInTheDocument();
-});
-
+		expect(getByText('1')).toBeInTheDocument();
+	});
 });
