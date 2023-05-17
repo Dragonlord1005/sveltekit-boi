95a093ee4f5dc64ea6ca5adda64d2ac82a80b53a
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-05-17 16:43:30 +0000

    🧪Failing test

--- src/routes/Counter.test.ts
+++ src/routes/Counter.test.ts
@@ -1,17 +1,33 @@
 import { describe, it, expect } from 'vitest';
-import { Counter } from './Counter.svelte';
+import { render, screen } from '@testing-library/svelte';
+import Counter from './Counter.svelte';
+import userEvent from '@testing-library/user-event';
 
 describe('Counter', () => {
     it('should render', () => {
+        render(Counter);
         expect(Counter).toMatchSnapshot();
     });
 
-    it('should be able to increment', () => {
-        const { component, getByText } = Counter.render();
-        getByText('0');
+    it('should be able to increment', async () => {
+        const user = userEvent.setup()
 
-        component.$set({ count: 1 });
-        getByText('1');
+        const { component } = render(Counter);
+
+        // Mock the increment function
+        let count = 0;
+        const mock = vi.fn((event) => {
+            count = event.detail.count;
+            console.log('count', count);
+        });
+        component.$on('increment', mock);
+
+        const button = screen.getByRole('button', { name: /increase the counter by one/i });
+        await user.click(button);
+
+        // expect(mock).toHaveBeenCalled();
+        expect(count).toBe(1);
+
     });
 }
 );
