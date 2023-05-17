2399b239c2fe63148299c0fa73f00b02a55c2ed2
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-05-17 18:43:45 +0000

    ✅ Fixed failing increase count

--- src/routes/Counter.test.ts
+++ src/routes/Counter.test.ts
@@ -1,6 +1,6 @@
-import { render, fireEvent } from '@testing-library/svelte';
+import { render } from '@testing-library/svelte';
 // Switch to userEvent
-
+import userEvent from '@testing-library/user-event';
 import Counter from './Counter.svelte';
 
 test('renders', () => {
@@ -18,15 +18,13 @@ test('renders', () => {
 });
 
 test('increases count', async () => {
-  const { getByLabelText, getByText, getByTestId } = render(Counter);
+  const { getByLabelText, getByText } = render(Counter);
 
-  const CounterDigits = getByTestId('counter-digits');
   const increaseButton = getByLabelText('Increase the counter by one');
 
-  expect(CounterDigits).toHaveTextContent('0');
+  expect(getByText('0')).toBeInTheDocument();
 
-  await fireEvent.click(increaseButton);
+  await userEvent.click(increaseButton);
 
   expect(getByText('1')).toBeInTheDocument();
-  expect(CounterDigits).toHaveTextContent('1');
 });
